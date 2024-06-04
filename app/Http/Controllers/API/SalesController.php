e0985ec5a34a6451e56d83fd8895d6faf8980bb4
hiroyuki-azuma <azm1658120@gmail.com>
2024-06-04 22:17:17 +0900

    step8-5　api作成

---
+++ app/Http/Controllers/API/SalesController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+// 下記は追記
+// Productモデルを使用
+use App\Models\Product;
+// Saleモデルを使用
+use App\Models\Sale;
+
+class SalesController extends Controller {
+
+    public function purchase( Request $request ) {
+
+        // ① salesテーブルにレコードを追加する
+        // 自動販売機から「売れた商品 = 商品ID」の情報を取得する
+        $soldItemsID = $request->input( 'product_id' );
+
+        // ② productsテーブルの在庫数を減算する
+        // リクエスト（削除）された商品を一つ減らす
+        $sales = Product::where( 'id', $request->id )->decrementQuantity( 'stock', 1 );
+
+        // ③ 在庫が0の商品を購入しようとした場合、エラーとなり購入できないこと
+        // もしも在庫がない（０以下の）商品の場合、エラーにする
+        if ( $product->stock <= 0 ) {
+            return response()->json( '在庫がありません' );
+        }
+
+        // レコードを追加する。
+        // ① salesテーブルにレコードを追加する
+        $sales->save();
+        return response()->json( '購入成功' );
+
+    }
+
+}
+
+
